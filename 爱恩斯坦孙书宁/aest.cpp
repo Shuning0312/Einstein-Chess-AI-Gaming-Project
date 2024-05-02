0f03e8973f765c037c462e25ae65d8460a60cef2
Shuning0312 <61745848+Shuning0312@users.noreply.github.com>
2024-05-02 15:18:51 +0800

    Add files via upload

---
+++ 爱恩斯坦孙书宁/aest.cpp
@@ -0,0 +1,20 @@
+#include"aest.h"
+
+int main()
+{
+    initwindow(460,520);
+    initBoard(board);
+    //checkGenMove();
+    //checkDistanceValue();
+    //checkEatValue();
+    //randomPlay(board,num);
+    //checkGetBestRandPosition();
+    //checkIsRightNum();
+    //checkRandomPlay();
+    //checkValue();
+    //checkMinMaxSearch();
+    //checkNegaMax();
+    //checkGetCountForValue();
+    //checkAlphaBeta();
+    return 0;
+}
